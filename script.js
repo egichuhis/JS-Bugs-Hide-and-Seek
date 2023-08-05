18e2e926938183b3d130f75f523bd57f0cc556d7
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-05 11:22:28 +0300

    JS code with bugs

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function addNumbers(a, b) {
-    return a - b; // Intentionally changed from a + b to a - b
+    return a - b;
 }
 
 const num1 = 10;
-const num2 = '5'; // Intentionally changed to a string
-
-const sum = addNumbers(num1, num2); // Intentionally passing a string
+const num2 = '5';
+const sum = addNumbers(num1, num2);
 console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
 
-const x = document.querySelector('result-of-sum'); // Intentionally missing #
+const x = document.querySelector('result-of-sum');
 x.innerHTML = `Sum of Numbers is: ${sum}`;
